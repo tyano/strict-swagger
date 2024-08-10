489a770e4247c23f66404186574db2fa65b192bc
Tsutomu YANO <tsutomu.yano@uzabase.com>
2024-08-10 21:52:50 +0900

    release version 1.0.0

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject org.clojars.t_yano/strict-swagger "0.1.0-SNAPSHOT"
+(defproject org.clojars.t_yano/strict-swagger "1.0.0"
   :description "The library to generate a map-data for ring-swagger from the validation spec of 'strict' library."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/tyano/strict-swagger"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[cheshire "5.13.0"]]
   :profiles 
   {:provided 
    {:dependencies [[org.clojars.t_yano/strict "2.0.0"]
